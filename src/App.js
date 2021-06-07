858437788f93a2be3955f1f5a0cfc3ee01471640
Aniket Chowdhury <aniket.bhanj@pnp-hcl.com>
2021-06-08 02:44:59 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,247 @@
-import logo from './logo.svg';
 import './App.css';
+import { Select, MenuItem, Typography, FormControl, makeStyles, Paper, TableRow, TableCell, Table, TableHead,TableBody, TableContainer, InputLabel, Snackbar, IconButton} from "@material-ui/core";
+import { useEffect, useState } from 'react';
+import {getStates, getDistricts, getSlots} from "./fetchAPI";
+import moment from "moment";
+import _ from "underscore";
+import CloseIcon from "@material-ui/icons/Close";
+import ErrorIcon from '@material-ui/icons/Error';
 
+const useStyles = makeStyles(theme=>({
+  root:{
+    margin: theme.spacing(1),
+    top:"50px",
+    display:"flex",
+    flexDirection:"row",
+    minWidth:"fit-content"
+  },
+  selectStyle:{
+    width:"300px"
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingTop: "50px",
+    height: "85vh"
+  },
+  colorCorrectAnswer:{
+    backgroundColor: "#92F7BA",
+    padding: "15px",
+    borderRadius: "7px",
+    color: "#474747"
+  },
+}))
 function App() {
+  const classes = useStyles();
+  let audio = new Audio("https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3");
+  const [stateName, setStateName] = useState("");
+  const [districtName, setDistrictName] = useState("");
+  const [stateCode, setStateCode] = useState(0);
+  const [districtCode, setDistrictCode] = useState(0);
+  const [stateList, setStateList] = useState([]);
+  const [districtList, setDistrictList] = useState([]);
+  const [slotsList, setSlotsList] = useState([]);
+  const [doseChoice, setDoseChoice] = useState("");
+  const [open, setOpen] = useState(true);
+  const vertical='bottom';const horizontal='center';
+
+  useEffect(()=>{
+    async function fetchData(){
+      const listOfStates = await getStates();
+      setStateList(listOfStates);
+    }
+    fetchData();
+  }, []);
+
+  useEffect(()=>{
+    const fetchDistricts=async(ID)=>{
+      const listOfDistricts = await getDistricts(ID);
+      setDistrictList(listOfDistricts);
+    }
+    fetchDistricts(stateCode);
+  }, [stateCode]);
+
+  const displaySlots = ()=>{
+    let centername; let sessions=[]; let freeSlots=[] ;
+    slotsList.forEach(item=>{
+      sessions = item.sessions;
+      sessions.forEach(val=>{
+        let dose = _.isEqual(doseChoice,"dose1") ? val.available_capacity_dose1 : val.available_capacity_dose2;
+        if(dose > 0)
+          {
+            centername = item.name;
+            let obj={openSlots:0,centerName:"",slotDate:"",vaccine:""};
+            obj.openSlots = dose;
+            obj.centerName = centername;
+            obj.slotDate = val.date;
+            obj.vaccine = val.vaccine;
+            freeSlots.push(obj);
+          }
+      })
+    })
+    if(!_.isEmpty(freeSlots)){
+      audio.play();
+      return(
+        <TableContainer component={Paper} style={{marginTop: "60px",
+          width: "650px",
+          height: "420px",
+          opacity: "0.87",
+          overflowY: "auto"}}>
+        <Table size="medium" stickyHeader aria-label="sticky table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Center Name</TableCell>
+            <TableCell align="left">Open Slots</TableCell>
+            <TableCell align="left">Slot Dates&nbsp;</TableCell>
+            <TableCell align="left">Vaccine&nbsp;</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {freeSlots.map((item,index)=>{
+          return (<TableRow key={index.toString()}>
+            <TableCell>{item.centerName}</TableCell>
+            <TableCell>{item.openSlots}</TableCell>
+            <TableCell>{item.slotDate}</TableCell>
+            <TableCell>{item.vaccine}</TableCell>
+            </TableRow>
+          )
+        })
+      }
+        </TableBody>
+      </Table>
+      </TableContainer>
+      )
+    }
+    else
+    return (
+      <div style={{display:"flex",marginTop: "100px"}}>
+        <ErrorIcon fontSize="medium" style={{width:"4.5rem" ,height:"2.5rem", color:"crimson"}}/>
+        <Typography variant="h4">No Slots Found</Typography>
+    </div>
+    )
+  }
+
+  useEffect(()=>{
+    const fetchSlots = async(ID,date) => {
+      const listOfSlots = await getSlots(ID,date);
+      setSlotsList(listOfSlots);
+    }
+    let date = moment().format('DD-MM-YYYY');
+    fetchSlots(districtCode,date);
+    const interval = setInterval(() => fetchSlots(districtCode,date), 60000)
+        return () => {
+          clearInterval(interval);
+        }
+  }, [districtCode]);
+
+  const handleStateSelect = (event) =>{
+     setStateName(event.target.value);
+     let stateID = stateList.find(item=>item.state_name===event.target.value).state_id;
+     setStateCode(stateID);
+  }
+
+  const handleDistrictSelect = (event) => {
+    setDistrictName(event.target.value);
+    let districtID = districtList.find(item=> item.district_name===event.target.value).district_id;
+    setDistrictCode(districtID);
+    displaySlots();
+  }
+
+  const handleDoseSelect = (event) => {
+    setDoseChoice(event.target.value);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={classes.wrapper}>
+        <Typography variant="h3">Cowin Slot Finder</Typography>
+        <div style={{display:"flex"}}>
+        <FormControl variant="outlined" className={classes.root}>
+        {stateList && (
+          <>
+          <InputLabel style={{top:"-6px"}}>Select your state</InputLabel>
+          <Select variant="outlined" 
+            className={classes.selectStyle} 
+            value={stateName} 
+            onChange={handleStateSelect}
+            MenuProps={{
+              style: {top:"50px",height:"350px"}
+            }}
+          >
+          {
+            stateList.map((item,index)=>(
+              <MenuItem value={item.state_name} key={index.toString()}>{item.state_name}</MenuItem>
+            ))
+          }
+        </Select>
+        </>
+        )
+        }
+        </FormControl>
+        <FormControl variant="outlined" className={classes.root}>
+        {stateName && districtList && (
+          <>
+          <InputLabel style={{top:"-6px"}}>Select your district</InputLabel>
+          <Select variant="outlined" 
+            className={classes.selectStyle} 
+            value={districtName} 
+            onChange={handleDistrictSelect}
+            MenuProps={{
+              style: {top:"50px",height:"350px"}
+            }}
+          >
+          {
+            districtList.map((item,index)=>(
+              <MenuItem 
+                value={item.district_name} 
+                key={index.toString()}
+              >
+                {item.district_name}
+              </MenuItem>
+            ))
+          }
+        </Select>
+        </>
+        )
+        }
+        </FormControl><FormControl variant="outlined" className={classes.root}>
+        {stateName && districtList && (
+          <>
+          <InputLabel style={{top:"-6px"}}>Select your dose</InputLabel>
+          <Select variant="outlined" 
+            className={classes.selectStyle} 
+            value={doseChoice} 
+            onChange={handleDoseSelect}
+            MenuProps={{
+              style: {top:"50px",height:"350px"}
+            }}
+          >
+          <MenuItem value={'dose1'}>DOSE 1</MenuItem>
+          <MenuItem value={'dose2'}>DOSE 2</MenuItem>
+        </Select>
+        </>
+        )
+        }
+        </FormControl>
+        </div>
+        {!_.isEmpty(slotsList) && !_.isEmpty(doseChoice) && displaySlots()}
+      </div>
+      <Snackbar
+        open={open}
+        anchorOrigin={{vertical, horizontal}}
+        message="Data is refreshed every 1 minute"
+        action={
+          <IconButton color="inherit" size="small" onClick={handleClose}>
+            <CloseIcon fontSize="small"/>
+          </IconButton>
+        }
+      >
+      </Snackbar>
     </div>
   );
 }
---
+++ src/fetchAPI.js
@@ -0,0 +1,45 @@
+const URLStates = "https://cdn-api.co-vin.in/api/v2/admin/location/states";
+
+export const getStates = async() =>{
+    const requestOptions={
+    method: "GET"
+}
+    const res = await fetch(URLStates,requestOptions);
+    if(res.ok)
+    {
+        return res.json().then(data=>{
+            console.log(data.states);
+            return data.states;
+        })
+    }
+}
+
+export const getDistricts = async(stateID)=>{
+    const requestOptions={
+        method: "GET"
+    }
+    const URLDistrictList = `https://cdn-api.co-vin.in/api/v2/admin/location/districts/`+stateID;
+    const results = await fetch(URLDistrictList,requestOptions);
+    if(results.ok)
+    {
+        return results.json().then(res=>{
+            console.log(res.districts);
+            return res.districts;
+        })
+    }
+}
+
+export const getSlots = async(ID,date) => {
+    const requestOptions={
+        method: "GET"
+    };
+    const URL = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=`+ID+`&date=`+date;
+    const results = await fetch(URL, requestOptions);
+    if(results.ok)
+    {
+        return results.json().then(res=>{
+            console.log(res.centers);
+            return res.centers;
+        })
+    }
+}
